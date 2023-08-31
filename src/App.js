faa422970ad45e8e93afdb1f61b1c23ecb4edfc1
[Rajeev ui] <[rajeevbaki30@gmail.com]>
2023-08-31 13:02:29 -0500

    Add code to fetch API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState('');
+  const [states, setStates] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setTimeout(async () => {
+          const response = await axios.get(
+            'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest'
+          );
+          const stateData = response.data.data;
+          setStates(stateData);
+          setLoading(false);
+        }, 2000); // Added a 2 second delay to see the API status change from 'Loading' to 'Completed'
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  let filteredStates = states;
+
+  if (filter !== '') {
+    filteredStates = states.filter((state) =>
+      state.State.toLowerCase().startsWith(filter.toLowerCase())
+    );
+    filteredStates.sort((a, b) => a.State.localeCompare(b.State)).reverse();
+  } else {
+    filteredStates.sort((a, b) => a.Population - b.Population);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>US Population Data</h1>
+        <p>Status: {loading ? 'Loading...' : 'Completed'}</p>
+        <label>
+          Filter:
+          <input type="text" value={filter} onChange={handleFilterChange} />
+        </label>
+        <ul>
+          {filteredStates.map((state) => (
+            <li key={state.State}>
+              {state.State} ({state.Population})
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
